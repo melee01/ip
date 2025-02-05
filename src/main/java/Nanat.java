64f0305a55cfe60edfdaa6708ea876e93cad0491
melee01 <melisakomputer@gmail.com>
2025-02-05 17:56:05 +0800

    Level-0

---
+++ src/main/java/Nanat.java
@@ -0,0 +1,14 @@
+public class Nanat {
+    public static void main(String[] args) {
+        String logo = """
+                __  __            __  __            _______
+                | \\ | |    / \\    | \\ | |     / \\   |_   _|
+                |  \\| |   / _ \\   |  \\| |    / _ \\    | |
+                | . ` |  / ___ \\  | . ` |   / ___ \\   | |
+                |_|\\ _| /_/   \\_\\ |_|\\ _|  /_/   \\_\\  |_|
+                """;
+
+        System.out.println("Hello I'm\n" + logo + "\nWhat can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
